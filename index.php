79d6fb9c131d1effda0c2f6845c3f670db7cf72c
Ethan Shernan <eshernan3@gatech.edu>
2013-04-11 21:28:04 -0400

    Adding initial test file

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<?php
+
+// Inialize session session_start();
+// Check, if user is already login, then jump to secured page if (isset($_SESSION['username'])) { header('Location: securedpage.php'); }
+?> <html>
+<head> <title>GT Car Rental</title> </head>
+<body><center>
+<h3>Login</h3>
+<table border="0"> <form method="POST" action="loginproc.php"> <tr><td>Username</td><td>:</td><td><input type="text" name="username" size="20"></td></tr> <tr><td>Password</td><td>:</td><td><input type="password" name="password" size="20"></td></tr> <tr><td>&nbsp;</td><td>&nbsp;</td><td><input type="submit" value="Login"></td></tr> </form> </table>
+</center>
+</body>
+</html>
