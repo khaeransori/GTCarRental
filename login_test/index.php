9a79a309bb81bd9f385e20f5957137d2bc308948
Derek <derekm@gatech.edu>
2013-04-14 21:59:56 -0400

    Cleanup

--- login_test/index.php
+++
@@ -1,7 +0,0 @@
-<form action="verify.php" method="post">
-    User Name:<br>
-    <input type="text" name="username"><br><br>
-    Password:<br>
-    <input type="password" name="password"><br><br>
-    <input type="submit" name="submit" value="Login">
-</form>
--- login_test/users_page.php
+++
@@ -1,8 +0,0 @@
-<?php 
-session_start(); 
-if(!$_SESSION['logged']){ 
-    header("Location: login_page.php"); 
-    exit; 
-} 
-echo 'Welcome, '.$_SESSION['username']; 
-?>
