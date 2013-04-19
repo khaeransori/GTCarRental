4b4653298eaa343517c1f18ddf7669a8ec09216f
Ethan Shernan <eshernan3@gatech.edu>
2013-04-19 14:18:07 -0400

    Commented out createAccountProcess, still working on it

--- createAccountProcess.php
+++ createAccountProcess.php
@@ -1,4 +1,8 @@
-<?php 
+<?php
+
+//Stuff for error reporting that I haven't gotten working yet...
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
 session_start(); 
 if(isset($_POST['Submit'])){ 
 
@@ -16,37 +20,38 @@ if(isset($_POST['Submit'])){
     $passConfirm = $_POST['passwordConfirm'];
     $userType = $_POST['userType'];
 
+	//Check if username exists
     $userExistsSql = mysql_query("SELECT * FROM User WHERE (Username = '$user')") or die(mysql_error());
 
     
     if(mysql_num_rows($userExistsSql) == 0){ //if user doesn't already exist
         if ($pass == $passConfirm) { //if passwords match
             //SQL Statements to insert user to databases
-            //TODO Ensure that the 2 sql statements below are "running"
             $insertUserSql = mysql_query("INSERT INTO User (Username, Password) VALUES ('$user', '$pass')") or die(mysql_error());
             
-	    if($userType == 'gtStudentFaculty'){
-            	mysql_query("INSERT INTO Member (Username) VALUES ('$user')");
-		$_SESSION['username'] = $user;
-	    	header('Location: personalInfo.php');
-	    }
-            else{
-		mysql_query("INSERT INTO Employee (Username) VALUES ('$user')");
-		$_SESSION['username'] = $user;
-		header('Location: employeeHome.php');
-	    }
-
-            exit; 
-        } else {
+            //If user is a student/faculty
+			if($userType == 'gtStudentFaculty'){
+				mysql_query("INSERT INTO Member (Username) VALUES ('$user')");
+				$_SESSION['username'] = $user;
+				header('Location: personalInfo.php');
+			}
+			//If user is an employee
+			else{
+				mysql_query("INSERT INTO Employee (Username) VALUES ('$user')");
+				$_SESSION['username'] = $user;
+				header('Location: employeeHome.php');
+			}
+	    
+        } 
+        else
              echo "Passwords didn't match";
-            exit;
     } 
     else {
         echo "User already exists";
-        exit;
     }
-}else{    //If the form button wasn't submitted go to the index page, or login page 
-    header('Location: createAccount.php'); //TODO verify that this is correct   
+}
+else{
+    header('Location: createAccount.php'); //shouldn't occur, might delete later 
     exit; 
-} 
+}
 ?>
