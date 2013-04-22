b73860f72ebfeb6994bdd2e465feb55ab889140a
Derek <derekm@gatech.edu>
2013-04-21 23:13:08 -0400

    Rental info history finished

--- rentalInfo.php
+++ rentalInfo.php
@@ -47,3 +47,36 @@
 <hr>
 
 <p><b>Previous Reservations </b></p> 
+
+<table border = "1">
+	<tr>
+		<th><font color="#ffffff">Reservation Date/Time</font></th>
+		<th><font color="#ffffff">Car</font></th>
+		<th><font color="#ffffff">Location</font></th>
+		<th><font color="#ffffff">Amount</font></th>
+		<th><font color="#ffffff">Return Status</font></th>
+	</tr>
+
+
+
+	<?php
+	$user = $_SESSION['username'];
+	$getHistory = mysql_query("SELECT Reservation.Username, Reservation.Pick_Up_Date_Time, Reservation.Estimated_Cost, Reservation.Return_Status, Car.Model, Car.Location_Name FROM Reservation
+		INNER JOIN Car
+		ON Reservation.Serial_Number = Car.Serial_Number
+		WHERE Reservation.Username = '$user' AND Reservation.Return_Date_Time < CURDATE()");
+
+	while ($temp = mysql_fetch_assoc($getHistory)) {
+		echo '<tr>';
+		echo '<td> <font color="#ffffff">'.$temp['Pick_Up_Date_Time'].'</font></td>';
+		echo '<td> <font color="#ffffff">'.$temp['Model'].'</font></td>';
+		echo '<td> <font color="#ffffff">'.$temp['Location_Name'].'</font></td>';
+		echo '<td> <font color="#ffffff">'.$temp['Estimated_Cost'].'</font></td>';
+		echo '<td> <font color="#ffffff">'.$temp['Return_Status'].'</font></td>';
+		echo '</tr>';
+	}
+	?>
+</table>
+
+
+
