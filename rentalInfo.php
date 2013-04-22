964b8683e49cd681d50d43f0051eb9ceb021f144
Derek <derekm@gatech.edu>
2013-04-21 23:19:55 -0400

    Current reservations implemented

--- rentalInfo.php
+++ rentalInfo.php
@@ -38,6 +38,47 @@
 
 <p><b>Current Reservations </b></p> 
 
+
+<table border = "1">
+	<tr>
+		<th><font color="#ffffff">Reservation Date/Time</font></th>
+		<th><font color="#ffffff">Car</font></th>
+		<th><font color="#ffffff">Location</font></th>
+		<th><font color="#ffffff">Amount</font></th>
+		<th><font color="#ffffff">Extend?</font></th>
+	</tr>
+
+
+
+	<?php
+	$user = $_SESSION['username'];
+	$getFutureReservations = mysql_query("SELECT Reservation.Username, Reservation.Pick_Up_Date_Time, Reservation.Estimated_Cost, Reservation.Return_Status, Car.Model, Car.Location_Name FROM Reservation
+		INNER JOIN Car
+		ON Reservation.Serial_Number = Car.Serial_Number
+		WHERE Reservation.Username = '$user' AND Reservation.Return_Date_Time > CURDATE()");
+
+	while ($temp = mysql_fetch_assoc($getFutureReservations)) {
+		echo '<tr>';
+		echo '<td> <font color="#ffffff">'.$temp['Pick_Up_Date_Time'].'</font></td>';
+		echo '<td> <font color="#ffffff">'.$temp['Model'].'</font></td>';
+		echo '<td> <font color="#ffffff">'.$temp['Location_Name'].'</font></td>';
+		echo '<td> <font color="#ffffff">'.$temp['Estimated_Cost'].'</font></td>';
+		echo '<td> <input type="radio" name="car" value="'.$temp['Serial_Number'].'"></td>';
+		echo '</tr>';
+	}
+	?>
+</table>
+
+
+
+
+
+
+
+
+
+
+
 <form method="post">
 	Chose a return time (YYYY-MM-DD HH:MM:SS format):
 	<input type="text" name="return"><br>
