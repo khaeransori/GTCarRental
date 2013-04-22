3470bd936acb471faaee2d58e2d62100310344a6
Derek <derekm@gatech.edu>
2013-04-21 21:06:36 -0400

    Date input implemented
    basic text entry as YYYY-MM-DD HH:MM:SS format

--- rentACar.php
+++ rentACar.php
@@ -34,67 +34,49 @@
  ?>
 
 
-<!-- ************************************************************* -->  
-       
-
-<form>
-Pick up time:
-<select name="pickupDate">
-<option value="jan1">1/1/2013</option>
-<option value="jan2">1/2/2013</option>
-</select>
-<select name="pickupTime">
-<option value="900">9:00 AM</option>
-<option value="930">9:30 AM</option>
-</select><br>
-
-Return time:
-<select name="returnDate">
-<option value="jan1">1/1/2013</option>
-<option value="jan2">1/2/2013</option>
-</select>
-<select name="returnTime">
-<option value="900">9:00 AM</option>
-<option value="930">9:30 AM</option>
-</select><br>
-
-Select Location:
-<select name="location">
-	<?php
-	//Lists the locations from the SQL table in the option list
-	$getLocations = mysql_query("SELECT Location_Name FROM Location");
-	while ($temp = mysql_fetch_assoc($getLocations)) {
-		echo "<option value='".$temp['Location_Name']."'>".$temp['Location_Name']."</option>";
-	}
-	?>
-</select><br>
-
-
-Cars Type:
-<select name="carType">
-	<?php
-	//Lists the car types from the SQL table in the option list
-	$getCarTypes = mysql_query("SELECT DISTINCT Type FROM Car");
-	while ($temp = mysql_fetch_assoc($getCarTypes)) {
-		echo "<option value='".$temp['Type']."'>".$temp['Type']."</option>";
-	}
-	?>
-</select>
-
-
-Car Model: 
-<select name="carType">
-	<?php
-	//Lists the car types from the SQL table in the option list
-	$getCarModels = mysql_query("SELECT DISTINCT Model FROM Car");
-	while ($temp = mysql_fetch_assoc($getCarModels)) {
-		echo "<option value='".$temp['Model']."'>".$temp['Model']."</option>";
-	}
-	?>
-</select><br>
-</form>
-
+<!-- ************************************************************* -->       
 
 <form action="carAvailability.php" method="post">
-    <input type="submit" name="search" value="Search">
+	Pick up date & time (YYYY-MM-DD HH:MM:SS format):
+	<input type="text" name="pickup"><br>
+
+	Return date & time (YYYY-MM-DD HH:MM:SS format):
+	<input type="text" name="return"><br>
+
+	Select Location:
+	<select name="location">
+		<?php
+		//Lists the locations from the SQL table in the option list
+		$getLocations = mysql_query("SELECT Location_Name FROM Location");
+		while ($temp = mysql_fetch_assoc($getLocations)) {
+			echo "<option value='".$temp['Location_Name']."'>".$temp['Location_Name']."</option>";
+		}
+		?>
+	</select><br>
+
+
+	Cars Type:
+	<select name="carType">
+		<?php
+		//Lists the car types from the SQL table in the option list
+		$getCarTypes = mysql_query("SELECT DISTINCT Type FROM Car");
+		while ($temp = mysql_fetch_assoc($getCarTypes)) {
+			echo "<option value='".$temp['Type']."'>".$temp['Type']."</option>";
+		}
+		?>
+	</select>
+
+
+	Car Model: 
+	<select name="carModel">
+		<?php
+		//Lists the car types from the SQL table in the option list
+		$getCarModels = mysql_query("SELECT DISTINCT Model FROM Car");
+		while ($temp = mysql_fetch_assoc($getCarModels)) {
+			echo "<option value='".$temp['Model']."'>".$temp['Model']."</option>";
+		}
+		?>
+	</select><br>
+
+	<input type="submit" name="search" value="Search">
 </form>
