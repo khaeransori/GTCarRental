dd42180693915c5e7141f600dfec9de992773c3e
Steven Wojcio <steven41292@gmail.com>
2013-04-21 21:17:01 -0400

    First attempt at listing available cars

--- carAvailability.php
+++ carAvailability.php
@@ -35,19 +35,59 @@ $dbHost = "academic-mysql.cc.gatech.edu";         //Location Of Database usually
 <table border = "1">
 	<tr>
 <th> </th>
-<th>Location</th>
-<th>Car Type</th>
+<th><font color="#ffffff">Model</font></th>
+<th><font color="#ffffff">Type</font></th>
+<th><font color="#ffffff">Location</font></th>
+<th><font color="#ffffff">Color</font></th>
+<th><font color="#ffffff">Hourly Rate (Occasional Driving Plan)</font></th>
+<th><font color="#ffffff">Discounted Rate (Frequent Driving Plan)</font></th>
+<th><font color="#ffffff">Discounted Rate (Daily Driving Plan)</font></th>
+<th><font color="#ffffff">Daily Rate</font></th>
+<th><font color="#ffffff">Seating Capacity</font></th>
+<th><font color="#ffffff">Transmission Type</font></th>
+<th><font color="#ffffff">Bluetooth Connectivity</font></th>
+<th><font color="#ffffff">Auxiliary Cable</font></th>
+<th><font color="#ffffff">Available Until</font></th>
+<th><font color="#ffffff">Estimated Cost</font></th>
 </tr>
 	<?php
 	//IMPORTANT CODE
 	//Lists the locations from the SQL table in the option list
-	$getCars = mysql_query("SELECT * FROM Car");
+	$getCars = mysql_query("SELECT * 
+		FROM Car AS c 
+		WHERE (
+		c.Location_Name =". $_POST['location'] ."AND 
+		((c.Type =". $_POST['carType'] .") OR (c.Model = ". $_POST['carModel'].")) AND 
+		c.Under_Maintenence_Flag = 0 AND
+		NOT EXISTS ( 
+			SELECT Serial_Number
+			FROM Reservation AS r 
+			WHERE(
+			(r.Pick_Up_Date_Time >=". $_POST['pickup']." AND r.Pick_Up_Date_Time<". $_POST['return'].") OR 
+			(r.Return_Date_Time >". $_POST['pickup']." AND r.Return_Date_Time<=". $_POST['return'].") OR 
+			(r.Pick_Up_Date_Time <=". $_POST['pickup']." AND r.Return_Date_Time>=". $_POST['return'].")
+		)
+		)
+		)");
 	while ($temp = mysql_fetch_assoc($getCars)) {
+		if ($temp["Under_Maintenence_Flag"]=="0"){
 		echo '<tr>';
 		echo '<td> <input type="radio" name="cars" value="'.$temp['Serial_Number'].'"></td>';
-		echo '<td> '.$temp['Serial_Number'].'</td>';
-		echo '<td> '.$temp['Model'].'</td>';
+		echo '<td> <font color="#ffffff">'.$temp['Model'].'</font></td>';
+		echo '<td> <font color="#ffffff">'.$temp['Type'].'</font></td>';
+		echo '<td> <font color="#ffffff">'.$temp['Location_Name'].'</font></td>';
+		echo '<td> <font color="#ffffff">'.$temp['Color'].'</font></td>';
+		echo '<td> <font color="#ffffff">'.$temp['Hourly_Rate'].'</td>';
+		echo '<td> <font color="#ffffff">THIS NEEDS TO BE THE FREQUENT RATE!!!!!!!!!</font></td>';
+		echo '<td> <font color="#ffffff">'.$temp['Daily_Rate'].'</font></td>';
+		echo '<td> <font color="#ffffff">'.$temp['Capacity'].'</font></td>';
+		echo '<td> <font color="#ffffff">'.$temp['Transmission_Type'].'</font></td>';
+		echo '<td> <font color="#ffffff">'.$temp['Bluetooth'].'</font></td>';
+		echo '<td> <font color="#ffffff">'.$temp['Aux_Cable'].'</font></td>';
+		echo '<td> <font color="#ffffff">AVAILABLE UNTIL GOES HERE!!!!!!!!!!!</font></td>';
+		echo '<td> <font color="#ffffff">ESTIMATED COST!!!!!!!!!</font></td>';
 		echo '</tr>';
+		}
 	}
 	?>
 	</table>
