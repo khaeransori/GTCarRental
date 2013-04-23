a430a46afe718ee7ce9a204ba340c015c2de119c
Derek <derekm@gatech.edu>
2013-04-22 20:02:44 -0400

    Verify rental info completed

--- rentalInfoVerify.php
+++ rentalInfoVerify.php
@@ -14,12 +14,13 @@
 //Take personal info and set it up into the database
 
 $returnTime = $_POST['return'];
+//$formattedDate = date('Y-m-d H:i:s',strtotime($returnTime));
 $reservationKey = $_POST['resPKey'];
 
 
 //TODO insert check to make sure that date format is correct
 //TYPE MISMATCH ON RETURN TIME. Need to fix and test
-mysql_query("UPDATE Reservation SET Return_Date_Time = '$return Time'
+mysql_query("UPDATE Reservation SET Return_Date_Time = '$returnTime'
 WHERE CONCAT(Username, Pick_Up_Date_Time, Return_Date_Time) = '$reservationKey'");
 
 header('Location: home.php');
