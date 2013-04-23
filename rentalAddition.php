ed2cea6b6e961ea8438b37903448672a9ac023b5
Steven Wojcio <steven41292@gmail.com>
2013-04-22 23:52:06 -0400

    Car availability now shows cars for all locations and organizes according to the requirements.

--- rentalAddition.php
+++ rentalAddition.php
@@ -16,9 +16,31 @@ $pickup = $_SESSION['pickup'];
 $return = $_SESSION['return'];
 $user = $_SESSION['username'];
 $loc = $_SESSION['loc'];
-//$r = new DateTime($return);
 
-mysql_query("INSERT INTO Reservation (Username, Pick_Up_Date_Time, Return_Date_Time, Late_Fees, Return_Status, Late_By, Estimated_Cost, Serial_Number, Location_Name) Values ('$user','$pickup','$return','0','0','0','$estCost','$serial','$loc')");
+$checkUser = mysql_result(mysql_query("SELECT Username 
+		FROM User AS u
+		WHERE (
+		u.Username ='". $user ."' AND
+		u.Username IN ( 
+			SELECT Username
+			FROM Reservation AS r 
+			WHERE(
+			(r.Pick_Up_Date_Time >='". $pickup."' AND r.Pick_Up_Date_Time<'". $return."') OR 
+			(r.Return_Date_Time >'".$pickup."' AND r.Return_Date_Time<='". $return."') OR 
+			(r.Pick_Up_Date_Time <='".$pickup."' AND r.Return_Date_Time>='". $return."')
+		)
+		)
+		)"),0);
+if ($checkUser== FALSE){
+	echo "<script type='text/javascript'>alert('Reservation Confirmed!');</script>";
+	mysql_query("INSERT INTO Reservation (Username, Pick_Up_Date_Time, Return_Date_Time, Late_Fees, Return_Status, Late_By, Estimated_Cost, Serial_Number, 	Location_Name) Values ('$user','$pickup','$return','0','0','0','$estCost','$serial','$loc')");
 
-header('Location: rentalInfo.php');
+	header('Location: rentalInfo.php');
+}
+else{
+	echo "<script type='text/javascript'>alert('You already have a reservation during that time, dummy!');</script>";
+	header('Location: rentACar.php');
+}
+
+
 ?>
